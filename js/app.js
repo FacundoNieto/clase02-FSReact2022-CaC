cefe370194093ce771ecaa34ad03fe6058d91ed8
Facundo Nieto <facundonieto24@gmail.com>
2022-08-29 20:44:05 -0300

    completo

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//A partir de ES6. React hace uso de las características del estandar ES6
+//Por eso es importante usar esta versión y no anteriores
+//(posiblemente sí posteriores)
+let myVar = 0;
+var myVar2 = 1;
+
+const myDom = document;
+
+console.log(myDom);
